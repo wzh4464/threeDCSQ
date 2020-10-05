ac329d89fe3d3045a4046652d1f4b68bbc95047d
chiellini <18319277690@163.com>
2020-10-05 14:19:19 +0800

    FALSE TO DRAW CURSE ****

--- functional_func/draw_func.py
+++ functional_func/draw_func.py
@@ -1,18 +1,89 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import MaxNLocator
+import scipy.spatial as sci_spatial
+from scipy.interpolate import griddata
+import pandas as pd
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+
+
+def generate_2D_Z_ARRAY(x, y, z):
+    x_num = len(x)
+    y_num = len(y)
+    Z = np.zeros((x_num, y_num), dtype=np.float64)
+    for i in range(x_num):
+        for j in range(y_num):
+            if i == j:
+                Z[i][j] = z[i]
+            else:
+                Z[i][j] = np.float64(0)
+    return Z
 
 
 def draw_3D_curve(points_data, fig_name="DEFAULT", fig_size=(6, 6)):
     fig = plt.figure(figsize=fig_size)
     ax = Axes3D(fig)
 
-    x = points_data[:, 0]  # first column of the 2D matrix
-    y = points_data[:, 1]
-    z = points_data[:, 2]
+    # x = points_data[:, 0]  # first column of the 2D matrix
+    # y = points_data[:, 1]
+    # z = points_data[:, 2]
 
-    # ax.plot_surface(x, y, z, rstride=1, cstride=1, cmap='rainbow')
-    ax.plot_trisurf(x, y, z, cmap=plt.cm.Spectral,antialiased=True)
+    num_points_half = int(len(points_data) / 2)
+    num_points_quarter = int(len(points_data) / 4)
+
+    points_data = points_data[points_data[:, 0].argsort()]
+    print(points_data.shape)
+    print(points_data)
+    print(points_data[0:num_points_half, :])
+
+    points_data_xnegative = points_data[:num_points_half, :]
+    points_data_xnegative = points_data_xnegative[points_data_xnegative[:, 1].argsort()]
+    points_data_xnegative_ynegative = points_data_xnegative[:num_points_quarter]
+    points_data_xnegative_ynegative = points_data_xnegative_ynegative[points_data_xnegative_ynegative[:, 2].argsort()]
+    points_data_xnegative_ypositive = points_data_xnegative[num_points_quarter:]
+    points_data_xnegative_ypositive = points_data_xnegative_ypositive[points_data_xnegative_ypositive[:, 2].argsort()]
+
+    points_data_xpositive = points_data[num_points_half:]
+    points_data_xpositive = points_data_xpositive[points_data_xpositive[:, 1].argsort()]
+    points_data_xpositive_ynegative = points_data_xpositive[:num_points_quarter]
+    points_data_xpositive_ynegative = points_data_xpositive_ynegative[points_data_xpositive_ynegative[:, 2].argsort()]
+    points_data_xpositive_ypositive = points_data_xpositive[num_points_quarter:]
+    points_data_xpositive_ypositive = points_data_xpositive_ypositive[points_data_xpositive_ypositive[:, 2].argsort()]
+
+    print(points_data_xpositive_ypositive)
+
+    x = points_data_xnegative_ynegative[:, 0]  # first column of the 2D matrix
+    y = points_data_xnegative_ynegative[:, 1]
+    z = points_data_xnegative_ynegative[:, 2]
+    ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+
+    x = points_data_xnegative_ypositive[:, 0]  # first column of the 2D matrix
+    y = points_data_xnegative_ypositive[:, 1]
+    z = points_data_xnegative_ypositive[:, 2]
+    ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+
+    x = points_data_xpositive_ynegative[:, 0]  # first column of the 2D matrix
+    y = points_data_xpositive_ynegative[:, 1]
+    z = points_data_xpositive_ynegative[:, 2]
+    ax.plot_trisurf(x, y, z, linewidth=0.2, antialiased=True)
+
+    x = points_data_xpositive_ypositive[:, 0]  # first column of the 2D matrix
+    y = points_data_xpositive_ypositive[:, 1]
+    z = points_data_xpositive_ypositive[:, 2]
+
+    xi = np.linspace(min(x), max(x))
+    yi = np.linspace(min(y), max(y))
+    X, Y = np.meshgrid(xi, yi)
+    Z = griddata(x, y, z, xi, yi)
+
+    surf = ax.plot_surface(X, Y, Z, rstride=5, cstride=5, cmap=cm.jet,
+                           linewidth=1, antialiased=True)
+
+    ax.set_zlim3d(np.min(Z), np.max(Z))
+    fig.colorbar(surf)
+
 
     ax.set_zlabel('Z')  # 坐标轴
     ax.set_ylabel('Y')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import functional_func.spherical_func as sphe_pack
 
 def main():
     print("start cell shape analysis")
-    spherical_fibonacci = sphe_pack.fibonacci_sphere(500, 10)
+    spherical_fibonacci = sphe_pack.fibonacci_sphere(500)
     draw_pack.draw_3D_curve(spherical_fibonacci)
     draw_pack.draw_3D_points(spherical_fibonacci)
 
