452f155116b094fc1f1cc9cba7af6c869a4dd4bc
chiellini <18319277690@163.com>
2022-08-25 11:37:54 +0800

    Work on window error checking 1. merge config and test_shape_analysis.py Following work: 1.

--- static/config.py
+++ static/config.py
@@ -1,5 +1,3 @@
-data_path = r'D:\cell_shape_quantification\DATA\\'
-
 # ----------LINUXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX---------------
 
 data_linux_CMAP_seg = r'/home/home/ProjectCode/LearningCell/MembProjectCode/output/'
@@ -12,7 +10,7 @@ data_label_name_dictionary= r"/home/home/ProjectCode/LearningCell/MembProjectCod
 
 data_stat_tem=r'/home/home/ProjectCode/LearningCell/CellShapeAnalysis/DATA/cell_mesh_contact/tem/'
 
-cell_shape_analysis_data_path=r'/home/jeff/ProjectCode/LearningCell/CellShapeAnalysis/DATA'
+linux_cell_shape_analysis_data_path=r'/home/jeff/ProjectCode/LearningCell/CellShapeAnalysis/DATA'
 
 data_stat=r"/home/home/ProjectCode/LearningCell/MembProjectCode/statistics/"
 
@@ -20,3 +18,40 @@ segmentation_by_jf=r'/home/jeff/ProjectCode/LearningCell/MembProjectCode/output/
 
 cmap_data_original=r'/home/jeff/ProjectCode/LearningCell/MembProjectCode/dataset/test'
 
+# -----------Lab path windowsssssssssssssssssssssssssssssssssssssss--------------------
+
+win_cell_shape_analysis_data_path = r'D:\cell_shape_quantification\DATA'
+
+data_win_CShaper_seg= r'D:\cell_shape_quantification\DATA\Segmentation Results'
+
+data_CMAP_seg = r'D:\MembraneProjectData\GUIData\WebData_CMap_cell_label_v1'
+
+data_cell_mesh_and_contact=r'C:\Users\zelinli6\OneDrive - City University of Hong Kong - Student\DATA\cell_mesh_contact'
+
+dir_segemented_tmp1 = win_cell_shape_analysis_data_path + r'Embryo04LabelUnified'
+
+dir_my_data = win_cell_shape_analysis_data_path + r'my_data'
+
+dir_my_data_R_matrix_csv = win_cell_shape_analysis_data_path + r'my_data_csv/R_matrix'
+
+dir_my_data_SH_PCA_csv = win_cell_shape_analysis_data_path + r'my_data_csv/SH_PCA_csv'
+
+dir_my_data_all_embryo = win_cell_shape_analysis_data_path + r'SegmentCellUnified04-20'
+
+dir_my_data_SH_time_domain_csv = win_cell_shape_analysis_data_path + r'my_data_csv/SH_time_domain_csv'
+
+dir_my_data_volume_surface = win_cell_shape_analysis_data_path + r'my_data_csv/volume_and_surface'
+
+dir_my_data_SH_clustering_csv = r'./DATA/my_data_csv/SH_clustering'
+
+dir_my_data_SHcPCA_ratio = r'./DATA/my_data_csv/SHcPCA_ratio'
+
+dir_my_regular_shape_path = r'./DATA\my_data_csv\regular_shape'
+
+dir_my_data_err_est_dir = r'./DATA\my_data_csv\err_estimation'
+
+shcpca_norm_path = r'./DATA/my_data_csv/SH_time_domain_csv/SHc_norm_PCA.csv'
+
+shcpca_path = r'./DATA/my_data_csv/SH_time_domain_csv/SHc_PCA.csv'
+
+cell_fate_path = r'./DATA/CellFate.xls'
--- test_shape_analysis.py
+++ test_shape_analysis.py
@@ -1,3 +1,4 @@
+# ------system or others' pack--------------
 import json
 import os
 import pickle
@@ -8,10 +9,12 @@ import pandas as pd
 import open3d as o3d
 from time import time
 import multiprocessing as mp
+import matplotlib.pyplot as plt
 
 from scipy import ndimage
 from tqdm import tqdm
 
+# -----user's package----------
 import static.config as my_config
 from utils.cell_func import get_cell_name_affine_table
 from utils.general_func import load_nitf2_img
@@ -72,63 +75,6 @@ def detect_outer_cells():
 
         print(df_volume_data)
         df_volume_data.to_csv(os.path.join(my_config.data_stat_tem, embryo_name + '_outerCell.csv'))
-
-
-# ['191108plc1p1', '200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1', '200326plc1p3', '200326plc1p4', '200122plc1lag1ip1', '200122plc1lag1ip2', '200117plc1pop1ip2', '200117plc1pop1ip3']
-
-def calculate_cell_surface_and_contact_points_CMap(is_calculate_cell_mesh=True, is_calculate_contact_file=True,
-                                                   showCellMesh=False,
-                                                   showCellContact=False):
-    """
-    I believe is is_calculate_cell_mesh and is_calculate_contact_file should be the same ,
-     i don't know what happen if they are not ( need to justify maybe)
-    just saving cell mesh and contact surface record
-    :param is_calculate_contact_file:
-    :param showCellMesh:
-    :param showCellContact:
-    :return:
-    """
-    # max_times = [205, 205, 255, 195, 195, 185, 220, 195, 195, 195, 140, 155]
-    max_times = [195, 140, 155]
-
-    # embryo_names = ['191108plc1p1', '200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1',
-    #                 '200326plc1p3', '200326plc1p4', '200122plc1lag1ip1', '200122plc1lag1ip2', '200117plc1pop1ip2',
-    #                 '200117plc1pop1ip3']
-    embryo_names = ['200122plc1lag1ip2', '200117plc1pop1ip2', '200117plc1pop1ip3']
-
-    # # --------TEST ONE EMBRYO-----------
-    # config_tmp = {}
-    # config_tmp["embryo_name"] = '191108plc1p1'
-    # config_tmp['is_calculate_cell_mesh'] = is_calculate_cell_mesh
-    # config_tmp['is_calculate_contact_file'] = is_calculate_contact_file
-    # config_tmp['showCellMesh'] = showCellMesh
-    # config_tmp['showCellContact'] = showCellContact
-    # config_tmp['time_point'] = 179
-    #
-    # calculate_cell_surface_and_contact_points(config_tmp,is_debug=True)
-    # # --------------------------------
-    # input()
-
-    for idx, embryo_name in enumerate(embryo_names):
-        configs = []
-        config_tmp = {}
-        config_tmp["embryo_name"] = embryo_name
-        config_tmp['is_calculate_cell_mesh'] = is_calculate_cell_mesh
-        config_tmp['is_calculate_contact_file'] = is_calculate_contact_file
-        config_tmp['showCellMesh'] = showCellMesh
-        config_tmp['showCellContact'] = showCellContact
-        for tp in tqdm(range(1, max_times[idx] + 1), desc="Compose configs"):
-            config_tmp['time_point'] = tp
-            configs.append(config_tmp.copy())
-
-        mpPool = mp.Pool(30)
-        # mpPool = mp.Pool(9)
-
-        for idx_, _ in enumerate(
-                tqdm(mpPool.imap_unordered(calculate_cell_surface_and_contact_points, configs), total=max_times[idx],
-                     desc="calculating {} segmentations (contact graph)".format(embryo_name))):
-            #
-            pass
 
 
 def calculate_cell_surface_and_contact_points(config_arg, is_debug=False):
@@ -422,6 +368,63 @@ def calculate_cell_surface_and_contact_points(config_arg, is_debug=False):
     # -------------------------------------------------------------------------------------------------------
 
 
+# ['191108plc1p1', '200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1', '200326plc1p3', '200326plc1p4', '200122plc1lag1ip1', '200122plc1lag1ip2', '200117plc1pop1ip2', '200117plc1pop1ip3']
+
+def calculate_cell_surface_and_contact_points_CMap(is_calculate_cell_mesh=True, is_calculate_contact_file=True,
+                                                   showCellMesh=False,
+                                                   showCellContact=False):
+    """
+    I believe is is_calculate_cell_mesh and is_calculate_contact_file should be the same ,
+     i don't know what happen if they are not ( need to justify maybe)
+    just saving cell mesh and contact surface record
+    :param is_calculate_contact_file:
+    :param showCellMesh:
+    :param showCellContact:
+    :return:
+    """
+    # max_times = [205, 205, 255, 195, 195, 185, 220, 195, 195, 195, 140, 155]
+    max_times = [195, 140, 155]
+
+    # embryo_names = ['191108plc1p1', '200109plc1p1', '200113plc1p2', '200113plc1p3', '200322plc1p2', '200323plc1p1',
+    #                 '200326plc1p3', '200326plc1p4', '200122plc1lag1ip1', '200122plc1lag1ip2', '200117plc1pop1ip2',
+    #                 '200117plc1pop1ip3']
+    embryo_names = ['200122plc1lag1ip2', '200117plc1pop1ip2', '200117plc1pop1ip3']
+
+    # # --------TEST ONE EMBRYO-----------
+    # config_tmp = {}
+    # config_tmp["embryo_name"] = '191108plc1p1'
+    # config_tmp['is_calculate_cell_mesh'] = is_calculate_cell_mesh
+    # config_tmp['is_calculate_contact_file'] = is_calculate_contact_file
+    # config_tmp['showCellMesh'] = showCellMesh
+    # config_tmp['showCellContact'] = showCellContact
+    # config_tmp['time_point'] = 179
+    #
+    # calculate_cell_surface_and_contact_points(config_tmp,is_debug=True)
+    # # --------------------------------
+    # input()
+
+    for idx, embryo_name in enumerate(embryo_names):
+        configs = []
+        config_tmp = {}
+        config_tmp["embryo_name"] = embryo_name
+        config_tmp['is_calculate_cell_mesh'] = is_calculate_cell_mesh
+        config_tmp['is_calculate_contact_file'] = is_calculate_contact_file
+        config_tmp['showCellMesh'] = showCellMesh
+        config_tmp['showCellContact'] = showCellContact
+        for tp in tqdm(range(1, max_times[idx] + 1), desc="Compose configs"):
+            config_tmp['time_point'] = tp
+            configs.append(config_tmp.copy())
+
+        mpPool = mp.Pool(30)
+        # mpPool = mp.Pool(9)
+
+        for idx_, _ in enumerate(
+                tqdm(mpPool.imap_unordered(calculate_cell_surface_and_contact_points, configs), total=max_times[idx],
+                     desc="calculating {} segmentations (contact graph)".format(embryo_name))):
+            #
+            pass
+
+
 def calculate_cell_surface_and_contact_points_CShaper(is_calculate_cell_mesh=True, is_calculate_contact_file=True,
                                                       showCellMesh=False,
                                                       showCellContact=False):
@@ -589,8 +592,121 @@ def display_cell_mesh_contact_CMap(is_showing_cell_mesh=False, is_showing_cell_c
     # 200322plc1p2,Caaaa,181
 
 
+def display_cell_mesh_contact_CShaper(is_show_original_points=False,is_showing_cell_mesh=False, is_showing_cell_contact=True):
+    # Sample05,ABpl,014
+    embryo_name, cell_name, tp = str(input()).split(',')
+    path_tmp = os.path.join(my_config.data_win_CShaper_seg, 'SegmentedCell', embryo_name + 'LabelUnified',
+                            '{}_{}_segCell.nii.gz'.format(embryo_name, tp))
+    this_img = load_nitf2_img(path_tmp)
+    volume = this_img.get_fdata().astype(int)
+    # print(np.unique(volume))
+
+    label_name_dict, name_label_dict = get_cell_name_affine_table(path=os.path.join(my_config.data_win_CShaper_seg, 'name_dictionary.csv'))
+    cell_idx = name_label_dict[cell_name]
+
+    # -------------------
+    cell_mask = volume != 0
+    boundary_mask = (cell_mask == 0) & ndimage.binary_dilation(cell_mask)
+    [x_bound, y_bound, z_bound] = np.nonzero(boundary_mask)
+    boundary_elements = []
+
+    # find boundary between cells
+    for (x, y, z) in zip(x_bound, y_bound, z_bound):
+        neighbors = volume[np.ix_(range(x - 1, x + 2), range(y - 1, y + 2), range(z - 1, z + 2))]
+        neighbor_labels = list(np.unique(neighbors))
+        neighbor_labels.remove(0)
+        if len(neighbor_labels) == 2:  # contact between two cells
+            boundary_elements.append(neighbor_labels)
+    # cell contact pairs
+    cell_contact_pairs = list(np.unique(np.array(boundary_elements), axis=0))
+    cell_conatact_pair_renew = []
+    contact_points_dict = {}
+    contact_area_dict = {}
+
+    # print(cell_contact_pairs)
+
+    for (label1, label2) in cell_contact_pairs:
+        # print(cell_idx,label1,label2)
+        if cell_idx in (label1, label2):
+            print('calculating ', (label1, label2), ' contact ')
+            contact_mask = np.logical_and(ndimage.binary_dilation(volume == label1),
+                                          ndimage.binary_dilation(volume == label2))
+            contact_mask = np.logical_and(contact_mask, boundary_mask)
+            if contact_mask.sum() > 2:
+
+                cell_conatact_pair_renew.append((label1, label2))
+                str_key = str(label1) + '_' + str(label2)
+                contact_area_dict[str_key] = 0
+
+                point_position_x, point_position_y, point_position_z = np.where(contact_mask == True)
+
+                contact_points_list = []
+                for i in range(len(point_position_x)):
+                    contact_points_list.append([point_position_x[i], point_position_y[i], point_position_z[i]])
+                # print(str_key)
+                contact_points_dict[str_key] = contact_points_list
+
+    contact_mesh_dict = {}
+    contact_sur_area = []
+    print((volume == cell_idx).sum())
+
+    print('calculating and saving', cell_idx, ' surface')
+    tuple_tmp = np.where(ndimage.binary_dilation(volume == cell_idx) == 1)
+
+    if is_show_original_points:
+        fig = plt.figure()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(tuple_tmp[0][:, None], tuple_tmp[1][:, None], tuple_tmp[2][:, None],s=1)
+        plt.show()
+
+    print(tuple_tmp)
+    sphere_list = np.concatenate(
+        (tuple_tmp[0][:, None], tuple_tmp[1][:, None], tuple_tmp[2][:, None]), axis=1)
+    sphere_list_adjusted = sphere_list.astype(float) + np.random.uniform(0, 0.001,
+                                                                         (len(tuple_tmp[0]), 3))
+    # print(np.unique(volume == cell_idx),tuple_tmp,sphere_list_adjusted)
+    m_mesh = generate_alpha_shape(sphere_list_adjusted, alpha_value=1, displaying=is_showing_cell_mesh)
+    print(len(m_mesh.cluster_connected_triangles()[2]))
+
+    # print('saving mesh')
+    # ============contact surface detection========================
+    cell_vertices = np.asarray(m_mesh.vertices).astype(int)
+    for (cell1, cell2) in cell_conatact_pair_renew:
+        idx = str(cell1) + '_' + str(cell2)
+        if cell_idx not in (cell1, cell2):
+            continue
+
+        # --------------------contact-----------------------------------------
+
+        # build a mask to erase not contact points
+        # enumerate each points in contact surface
+        contact_mask_not = [True for i in range(len(cell_vertices))]
+        contact_vertices_loc_list = []
+        for [x, y, z] in contact_points_dict[idx]:
+            # print(x,y,z)
+            contact_vertices_loc = np.where(np.prod(cell_vertices == [x, y, z], axis=-1))
+            if len(contact_vertices_loc[0]) != 0:
+                contact_vertices_loc_list.append(contact_vertices_loc[0][0])
+                contact_mask_not[contact_vertices_loc[0][0]] = False
+        contact_mesh_dict[idx] = contact_vertices_loc_list
+        contact_mesh = deepcopy(m_mesh)
+        contact_mesh.remove_vertices_by_mask(contact_mask_not)
+        contact_sur_area.append(contact_mesh.get_surface_area())
+
+        if is_showing_cell_contact:
+            print(label_name_dict[cell1], label_name_dict[cell2], ' contact pairt')
+            o3d.visualization.draw_geometries([contact_mesh], mesh_show_back_face=True,
+                                              mesh_show_wireframe=True)
+    print('cell volume', (volume == cell_idx).sum() * 0.25 ** 3, ' mesh caculation===>', m_mesh.get_volume())
+    print('cell surface area', m_mesh.get_surface_area() * 0.25 ** 2)
+    print('sum of cell contact area', sum(contact_sur_area) * 0.25 ** 2)
+    print('list of cell contact area', contact_sur_area)
+
+
+
 if __name__ == "__main__":
-    display_cell_mesh_contact_CMap(is_showing_cell_contact=True, is_showing_cell_mesh=True)
+    display_cell_mesh_contact_CShaper(is_show_original_points=True,is_showing_cell_contact=True, is_showing_cell_mesh=True)
+    # display_cell_mesh_contact_CMap(is_showing_cell_contact=True, is_showing_cell_mesh=True)
     # calculate_cell_surface_and_contact_points_CShaper()
     # detect_outer_cells()
     # calculate_cell_surface_and_contact_points(is_calculate_cell_mesh=False, is_calculate_contact_file=False,
